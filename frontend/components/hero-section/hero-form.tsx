7e7f80089b8532dbe856f2e56870ad8ce594d4d9
tshimegabelmont <tshimegabelmont@gmail.com>
2023-09-05 16:26:26 +0200

    Feat: Added zod validation to store creation form

--- frontend/components/hero-section/hero-form.tsx
+++ frontend/components/hero-section/hero-form.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { FC } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
--- frontend/components/modals/store-modal.tsx
+++ frontend/components/modals/store-modal.tsx
@@ -1,14 +1,51 @@
 "use client"
 
-import { FC } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
 
 import { useStoreModal } from "@/hooks/use-store-modal"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import Modal from "@/components/ui/modal"
 
-interface StoreModalProps {}
+import { ToastAction } from "../ui/toast"
+import { useToast } from "../ui/use-toast"
 
-const StoreModal: FC<StoreModalProps> = ({}) => {
+const storeFormSchema = z.object({
+  name: z.string().min(5, {
+    message: "Shop name must be at least 5 characters.",
+  }),
+})
+
+type StoreFormValues = z.infer<typeof storeFormSchema>
+
+const defaultValues: Partial<StoreFormValues> = {}
+
+export function StoreModal() {
   const storeModal = useStoreModal()
+
+  const form = useForm<StoreFormValues>({
+    resolver: zodResolver(storeFormSchema),
+    defaultValues,
+    mode: "onChange",
+  })
+  const { toast } = useToast()
+
+  function onSubmit(data: StoreFormValues) {
+    toast({
+      title: `New Store Created`,
+      description: `You can now add products to your ${data.name} shop.`,
+    })
+  }
+
   return (
     <Modal
       title="Create store"
