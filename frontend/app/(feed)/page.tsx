19e05d83baf2395d6c42f6b58b667d2915aa4003
tshimegabelmont <tshimegabelmont@gmail.com>
2023-09-10 12:03:28 +0200

    Minor UI Update

--- frontend/app/(feed)/page.tsx
+++ frontend/app/(feed)/page.tsx
@@ -7,32 +7,26 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 export default async function Feed() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-opacity-100 dark:bg-opacity-30 dark:transition-opacity dark:duration-500 items-center overflow-x-hidden">
-      <Tabs defaultValue="foryou" className="max-sm:max-w-xs">
-        <TabsList className="md:py-6 max-md:py-6 max-md:px-2 md:px-2">
-          <TabsTrigger value="foryou" className="text-xl md:text-2xl font-bold">
+      <Tabs defaultValue="foryou" className="max-md:w-auto">
+        <TabsList className="md:py-6 md:px-2">
+          <TabsTrigger value="foryou" className="md:text-2xl font-bold">
             For You
           </TabsTrigger>
-          <TabsTrigger
-            value="trending"
-            className="text-xl md:text-2xl font-bold"
-          >
+          <TabsTrigger value="trending" className="md:text-2xl font-bold">
             Discover
           </TabsTrigger>
-          <TabsTrigger
-            value="verified"
-            className="text-xl md:text-2xl font-bold"
-          >
+          <TabsTrigger value="verified" className="md:text-2xl font-bold">
             Verified
           </TabsTrigger>
         </TabsList>
         <TabsContent value="foryou" className="max-sm:max-w-xs">
-          Feed relevant to you
+          People and businesses relevant to you
         </TabsContent>
         <TabsContent value="trending" className="max-sm:max-w-xs">
-          Discover new people
+          Discover new people and businesses
         </TabsContent>
         <TabsContent value="verified" className="max-sm:max-w-xs">
-          Feed recommended for you
+          Verified people and businesses
         </TabsContent>
       </Tabs>
     </div>
